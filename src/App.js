24daddea2ce0425489184bbaedb8b5d44fe17672
Lauren Noble <lauren.noble3@gmail.com>
2023-03-15 20:22:22 -0400

    added react modal for instructions

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
 import './App.css';
 import { useState } from 'react';
 import ColorCard from './components/ColorCard';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
+
+const modalStyle = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
 
 function App() {
   const generateColors = () => {
     console.log('hello world');
   };
 
-  const [colorCardList, setColorCardList] = useState([])
+  const [colorCardList, setColorCardList] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
 
   const onAddBtnClick = () => {
-    setColorCardList(colorCardList.concat(<ColorCard />))
-  }
+    setColorCardList(colorCardList.concat(<ColorCard />));
+  };
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
   return (
     <div className="App">
+      <div id="nav">
+        <button id="instructions-btn" onClick={openModal}>
+          Welcome
+        </button>
+      </div>
+      <Modal
+        isOpen={modalOpen}
+        onRequestClose={closeModal}
+        style={modalStyle}
+        contentLabel="Instructions"
+      >
+        <h3>Welcome!</h3>
+        <p></p>
+      </Modal>
       <header>
         <h1>Color Picker</h1>
       </header>
